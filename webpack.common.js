d6b59dcde47e3acb07207c3c68b64cc3b81a69c6
Derek Wolpert <dfnj123@gmail.com>
2019-08-13 10:20:35 -0400

    swapped out outdated node with replacement

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const outputDir = "./dist";
 
 module.exports = {
@@ -23,19 +23,38 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    use: ["css-loader", "postcss-loader"],
-                    fallback: "style-loader"
-                })
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options:  {
+                            publicPath: "../",
+                            hmr: process.env.NODE_ENV === "development"
+                        }
+                    },
+                    "css-loader",
+                    "postcss-loader"
+                ]
             },
             {
                 test: /\.scss/,
-                use: ExtractTextPlugin.extract({
-                    use: ["css-loader", "sass-loader", "postcss-loader"],
-                    fallback: "style-loader"
-                })
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: "../",
+                            hmr: process.env.NODE_ENV === "development"
+                        }
+                    },
+                    "css-loader",
+                    "sass-loader",
+                    "postcss-loader"
+                ]
             }
         ]
     },
-    plugins: [new ExtractTextPlugin("[name].css"), require("autoprefixer")]
+    plugins: [new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+        ignoreOrder: false
+    }), require("autoprefixer")]
 };
